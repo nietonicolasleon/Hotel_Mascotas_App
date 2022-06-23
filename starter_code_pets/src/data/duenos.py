787355db6b56727d70dca7b7ce5c26b13e1fd35c
nietonicolasleon <nietonicolasleon@gmail.com>
2022-06-23 17:29:39 -0300

    Created duenos.py

---
+++ starter_code_pets/src/data/duenos.py
@@ -0,0 +1,12 @@
+class Dueno:
+    fecha_registro = None
+    nombre = None
+    email = None
+    
+    pet_ids = None
+    cucha_ids = None
+
+    meta ={
+        'db_alias' : 'core',
+        'collection' : 'duenos'
+    }
