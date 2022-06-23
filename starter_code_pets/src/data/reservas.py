0cd1ebb00778def0646488e4510c351e6ab108b3
nietonicolasleon <nietonicolasleon@gmail.com>
2022-06-23 17:17:51 -0300

    Created reservas.py

---
+++ starter_code_pets/src/data/reservas.py
@@ -0,0 +1,8 @@
+class Reserva:
+    guest_dueno_id = None
+    guest_pet_id = None
+    fecha_reserva = None
+    fecha_check_in = None
+    fecha_check_out = None
+    review = None
+    rating = None
